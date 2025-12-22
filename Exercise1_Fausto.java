945f985b356ebec6849c7be6c7df6fd94eab4f04
Christian Fausto <95928473+dagszzz@users.noreply.github.com>
2025-12-22 22:47:45 +0800

    Add files via upload
    Day 3 Exercise

---
+++ Exercise1_Fausto.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day4.activity;
+
+/**
+ *
+ * @author GG
+ */
+public class Exercise1_Fausto {
+   
+    public static void main(String[] args) {
+    boolean boolean_true = true;
+    boolean boolean_false = false;
+        System.out.println("This Boolean is " + boolean_true);
+        System.out.println("This Boolean is " + boolean_false);
+           
+    byte sample_numbyte = 64;
+        System.out.println("This is a sample of byte: " + sample_numbyte);    
+    
+    short sample_numshort = 128;
+        System.out.println("This is a sample of short: " + sample_numshort);  
+        
+    int sample_numint = 256;
+        System.out.println("This is a sample of int: " + sample_numint);  
+        
+    long sample_numlong = 512;
+        System.out.println("This is a sample of long: " + sample_numlong);    
+    
+    float sample_numfloat = 1024;
+        System.out.println("This is a sample of float: " + sample_numfloat);  
+        
+    double sample_numdouble = 2048;
+        System.out.println("This is a sample of double: " + sample_numdouble);  
+        
+    char sample_char = 'c';
+        System.out.println("This is a sample of character: " + sample_char);
+        
+    String sample_word = "word";
+        System.out.println("This is a sample of character: " + sample_word);
+    }
+}
+
