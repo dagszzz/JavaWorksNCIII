d92e40fe749eb4f714fa177888e57c6e6781e400
Christian Fausto <95928473+dagszzz@users.noreply.github.com>
2025-12-23 15:43:56 +0800

    Add files via upload

---
+++ Exercise2_Fausto.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day5.activity;
+
+/**
+ *
+ * @author GG
+ */
+public class Exercise2_Fausto {
+    public static void main(String[] args) {
+    
+        int SampleNum = 5;
+        
+        System.out.println(!(SampleNum % 2 != 0));
+        
+        System.out.println(SampleNum + (SampleNum % 2));
+        
+        
+    }
+    
+    
+}
